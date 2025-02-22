38dcf9b213cefa6369fa19145729d471956f9fde
Rbabar123 <108402307+Rbabar123@users.noreply.github.com>
2025-02-22 19:15:45 +0800

    Add files via upload

---
+++ visual-basic-Mysql/visual-basic-sql/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        openCon()
+        MsgBox("Connected")
+        con.Close()
+    End Sub
+End Class
---
+++ visual-basic-Mysql/visual-basic-sql/Module1.vb
@@ -0,0 +1,11 @@
+﻿Imports MySql.Data.MysqlClient
+
+Module Module1
+    Public con As New MySqlConnection
+
+    Sub openCon()
+        con.ConnectionString = "server=localhost;username=root;password=;database=db_tutorial"
+
+        con.Open()
+    End Sub
+End Module
